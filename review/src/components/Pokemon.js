243d8f58ef43415761ad8c9b4e1450b61a84c77d
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-13 16:36:00 -0500

    adds pokemon component.

---
+++ review/src/components/Pokemon.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Pokemon = ({ pokemon, handlePoke })=> {
+    return (<div
+        onClick={() => handlePoke(pokemon.id)}
+        key={pokemon.id}
+        className="pokemon"
+      >
+        <img src={pokemon.img} alt={pokemon.name} />
+        <div>
+          <h3>{pokemon.name}</h3>
+          {pokemon.next_evolution &&
+            pokemon.next_evolution.map((e) => <p key={e.num}>{e.name}</p>)}
+        </div>
+      </div>);
+}
+
+export default Pokemon;
