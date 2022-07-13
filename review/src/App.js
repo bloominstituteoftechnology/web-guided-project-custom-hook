8d6a499d861b781ce420779cfd4f5377210c21cd
RinatTr <rinatreg@gmail.com>
2022-07-13 13:11:58 -0400

    work in progress

--- review/src/App.js
+++ review/src/App.js
@@ -1,31 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./styles.scss";
 
-import { getPokemen, getSelectedPokemon } from './services/PokeServices'
+import usePokeState from './hooks/usePokeState'
+
 import PokeList from './components/PokeList';
 import SelectedPoke from './components/SelectedPoke';
-import data from "./../data";
-
-import axios from 'axios';
 
 function App() {
-  const usePokeState = (initPokemen, initSelectedPokemon) => {
-    const [pokemen, setPokemen] = useState(initPokemen);
-    const [selectedPokemon, setSelectedPokemon] = useState(initSelectedPokemon);
-  
-    useEffect(() => {
-      setPokemen(getPokemen());
-    }, []);
-  
-    const handlePoke = (id) => {
-     getSelectedPokemon(id)
-        .then((data) => {
-          setSelectedPokemon(data);
-        });
-    };  
-
-    return([selectedPokemon, handlePoke, pokemen])
-  }
 
   const [selectedPokemon, handlePoke, pokemen] = usePokeState([], {});
 
